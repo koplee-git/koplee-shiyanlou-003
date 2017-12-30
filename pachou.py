25ad9e3487544cc7272565e4481a24516abceaa8
503924173 <503924173@qq.com>
2017-12-30 23:17:06 +0800

    challenge-9

--- pachou.py
+++ pachou.py
@@ -13,18 +13,11 @@ class ShiyanlouCoursesSpider(scrapy.Spider):
             yield scrapy.Request(url=url,callback=self.parse)
 
     def parse(self,response):
-        
-        print('##########################')
-        for course in response.css('li.col-12.d-block.width-full.py-4.border-bottom.public.fork'):
-            print('****************')
-            
-            print(course.css('div[class="d-inline-block mb-1"] h3 a::text').re_first('\s*(\w+)'))
-            print('****************')
+        for course in response.css('li.col-12.d-block.width-full.py-4.border-bottom.public'):
             yield {
                     'name':course.css('div[class="d-inline-block mb-1"] h3 a::text').re_first('\s*(\w+)'),
 
                     'updata_time':course.css('div[class="f6 text-gray mt-2"] ::attr(datetime)').extract_first()
                     }
-
 
 
